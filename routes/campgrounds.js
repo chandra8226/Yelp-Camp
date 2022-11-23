95e010d002ce9e60b3dc1cc616c8df4d4f54bf11
Chandra Prakash <chandra8226@gmail.com>
2022-11-23 17:43:18 +0530

    Added Image upload, maps and basic styles

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 const campground = require('../controllers/campgrounds');
 const wrapAsync = require('../utils/wrapAsync');
@@ -6,14 +9,14 @@ const { isLoggedin, isAuthor, validateCampground } = require('../middlewares');
 
 const router = express.Router();
 
-router.route('/').get(campground.index).post(isLoggedin, validateCampground, wrapAsync(campground.createCampground));
+router.route('/').get(campground.index).post(isLoggedin, upload.array('image'), validateCampground, wrapAsync(campground.createCampground));
 
 router.get('/new', isLoggedin, campground.renderNewForm);
 
 router
   .route('/:id')
   .get(wrapAsync(campground.showCampground))
-  .put(isLoggedin, isAuthor, validateCampground, wrapAsync(campground.updateCampground))
+  .put(isLoggedin, isAuthor, upload.array('image'), validateCampground, wrapAsync(campground.updateCampground))
   .delete(isLoggedin, isAuthor, wrapAsync(campground.deleteCampground));
 
 router.get('/:id/edit', isLoggedin, isAuthor, wrapAsync(campground.renderEditForm));
--- seeds/index.js
+++ seeds/index.js
@@ -17,17 +17,27 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 200; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 100) + 10;
     const camp = new Campground({
       author: '636cd1d3ec1ea6f64ceba0fa',
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/483251',
       description:
         'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora tempore voluptatem fuga nobis non dolor consequuntur molestiae labore excepturi, iure qui mollitia molestias eveniet, provident, error dolorum. Itaque, ipsam soluta.',
       price,
+      geometry: { type: 'Point', coordinates: [cities[random1000].longitude, cities[random1000].latitude] },
+      images: [
+        {
+          url: 'https://res.cloudinary.com/dfsttffuh/image/upload/v1668421198/YelpCamp/unrdednevnaw52b7tqfs.jpg',
+          filename: 'YelpCamp/unrdednevnaw52b7tqfs',
+        },
+        {
+          url: 'https://res.cloudinary.com/dfsttffuh/image/upload/v1668421198/YelpCamp/gvfa5xk5e2ahgfgysvpp.jpg',
+          filename: 'YelpCamp/gvfa5xk5e2ahgfgysvpp',
+        },
+      ],
     });
     await camp.save();
   }
